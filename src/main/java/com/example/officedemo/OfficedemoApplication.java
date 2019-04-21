a13dfe2bff7282d54a6a2d407004436a93789f07
Sid <Sid@siddarths-mbp.fios-router.home>
2019-04-21 08:02:56 -0400

    initial commit

---
+++ src/main/java/com/example/officedemo/OfficedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.officedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficedemoApplication.class, args);
+	}
+
+}
